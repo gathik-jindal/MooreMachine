3a19aa3ad8413f561a0e3c264868a5fb7117c851
boom29 <Gathik.Jindal@iiitb.ac.in>
2023-12-16 14:41:02 +0530

    breh

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import pwlSource
 
 inputGen = pwlSource.InputGenerator("Tests\\Test.csv")
+print("-----printing .csv-------")
 inputs = inputGen.getInput()
 print(inputs)
 
 inputGen.setFilePath("Tests\\Test.txt")
+print("-----printing .txt-------")
 inputs = inputGen.getInput()
 print(inputs)
 
 inputGen.setFilePath("Tests\\Test.xlsx")
+print("-----printing .xlsx------")
 inputs = inputGen.getInput()
 print(inputs)
