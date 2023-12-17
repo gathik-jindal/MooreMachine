efb08703e6de81131aa076395bfea968027b0f45
AryanAAB <145079379+AryanAAB@users.noreply.github.com>
2023-12-17 08:48:10 +0530

    Add files via upload

--- scope.py
+++ scope.py
@@ -26,3 +26,25 @@ def plot(inputs: dict):
     plt.xlabel('Time (units)')
     plt.tight_layout()
     plt.show()
+
+if __name__ == "__main__":
+    import pwlSource
+    from scope import plot
+
+    inputGen = pwlSource.InputGenerator("Tests\\Test.csv")
+    print("-----printing .csv-------")
+    inputs = inputGen.getInput()
+    print(inputs)
+    plot(inputs)
+
+    inputGen.setFilePath("Tests\\Test.txt")
+    print("-----printing .txt-------")
+    inputs = inputGen.getInput()
+    print(inputs)
+    plot(inputs)
+
+    inputGen.setFilePath("Tests\\Test.xlsx")
+    print("-----printing .xlsx------")
+    inputs = inputGen.getInput()
+    print(inputs)
+    plot(inputs)
