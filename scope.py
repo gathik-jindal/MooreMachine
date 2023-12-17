530eff728190a8836e1440db613abb5ac2061f22
boom29 <Gathik.Jindal@iiitb.ac.in>
2023-12-17 09:38:00 +0530

    updated test

--- scope.py
+++ scope.py
@@ -36,20 +36,24 @@ def plot(inputs: dict):
 if __name__ == "__main__":
     import pwlSource
 
+    plot_dict = {}
+
     inputGen = pwlSource.InputGenerator("Tests\\Test.csv")
     print("-----printing .csv-------")
     inputs = inputGen.getInput()
     print(inputs)
-    plot(inputs)
+    plot_dict['inputs_csv'] = inputs['Inputs']
 
     inputGen.setFilePath("Tests\\Test.txt")
     print("-----printing .txt-------")
     inputs = inputGen.getInput()
     print(inputs)
-    plot(inputs)
+    plot_dict['inputs_txt'] = inputs['Inputs']
 
     inputGen.setFilePath("Tests\\Test.xlsx")
     print("-----printing .xlsx------")
     inputs = inputGen.getInput()
     print(inputs)
-    plot(inputs)
+    plot_dict['inputs_excel'] = inputs['Inputs']
+
+    plot(plot_dict)
