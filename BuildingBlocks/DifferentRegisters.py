f9a9afad9954f7db83fff8e959b84647ccc0fc9e
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-07-05 15:36:20 +0530

    Update DifferentRegisters.py

--- BuildingBlocks/DifferentRegisters.py
+++ BuildingBlocks/DifferentRegisters.py
@@ -62,6 +62,115 @@ class SISO:
     def __gt__(self,other):
         self.__register.output() > other
 
+
+class SIPO:
+    """
+    This class represents the SISO gate.
+    """
+
+    def __init__(self, pydig: pd, size:int, clock, delay: float, initialValue: int, plot: bool, blockID: str):
+        """
+        @param pydig : pydig object
+        @param delay : the time delay for each register in the SISO block.
+        @param size : the number of registers in the SISO block
+        @param initialValue : The initial output value given by each register in the block at t = 0 while running
+        @param plot : boolean value whether to plot this block or not
+        @param blockID : the id of this block. If None, then new unique ID is given.
+        """
+        checkType([(pydig, pd), (delay, (float, int)), (size, (int)), (initialValue, int), (plot, bool), (blockID, str)])
+        self.__register = pydig.moore(maxOutSize=size, plot=plot, blockID=blockID, startingState=initialValue, clock = clock, register_delay = delay)
+        self.__size = size
+        self.__register.nsl = self.__nsl
+        self.__register.ol = self.__ol
+
+    def __nsl(self, ps,i):
+        return ((ps*2)%(2**self.__size) + (i&1)) 
+
+    def __ol(self, ps):
+        k = self.__size
+        ans = 0
+        while(k):
+            k-=1
+            ans += ps&1
+            ps = ps >> 1
+        return ans
+    
+    def input(self, left=None, right=None):
+        """
+        @return obj : instance of the first register object
+        """
+        return self.__register.input(left,right)
+    
+    def output(self, left=0, right=None):
+        """
+        @return obj : instance of the last register object
+        """
+        return self.__register.output(left, right)
+
+    def clock(self):
+        """
+        @return obj : instance of the clock object
+        """
+        return self.__register.clock()
+    
+    def __le__(self, other):
+        self.__register.input() <= other
+
+    def __gt__(self,other):
+        self.__register.output() > other
+
+
+class PIPO:
+    """
+    This class represents the SISO gate.
+    """
+
+    def __init__(self, pydig: pd, size:int, clock, delay: float, initialValue: int, plot: bool, blockID: str):
+        """
+        @param pydig : pydig object
+        @param delay : the time delay for each register in the SISO block.
+        @param size : the number of registers in the SISO block
+        @param initialValue : The initial output value given by each register in the block at t = 0 while running
+        @param plot : boolean value whether to plot this block or not
+        @param blockID : the id of this block. If None, then new unique ID is given.
+        """
+        checkType([(pydig, pd), (delay, (float, int)), (size, (int)), (initialValue, int), (plot, bool), (blockID, str)])
+        self.__register = pydig.moore(maxOutSize=size, plot=plot, blockID=blockID, startingState=initialValue, clock = clock, register_delay = delay)
+        self.__size = size
+        self.__register.nsl = self.__nsl
+        self.__register.ol = self.__ol
+
+    def __nsl(self, ps, i):
+        return i&(2**self.__size-1)
+
+    def __ol(self, ps):
+        return ps&(2**self.__size-1)
+    
+    def input(self, left=None, right=None):
+        """
+        @return obj : instance of the first register object
+        """
+        return self.__register.input(left,right)
+    
+    def output(self, left=0, right=None):
+        """
+        @return obj : instance of the last register object
+        """
+        return self.__register.output(left, right)
+
+    def clock(self):
+        """
+        @return obj : instance of the clock object
+        """
+        return self.__register.clock()
+    
+    def __le__(self, other):
+        self.__register.input() <= other
+
+    def __gt__(self,other):
+        self.__register.output() > other    
+
+
 if __name__ == "__main__":
     
     pysim = pd("Basic Gates")
